b88f68094bc4754bb4c94b9cace7c2a2f494f51f
Katsyaryna Anikevich <anick2901@gmail.com>
2025-03-15 20:23:06 +0100

    added collision box

---
+++ Source/CryptStalker/TriggerComponent.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+    UE_LOG(LogTemp, Error, TEXT("i live"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    UE_LOG(LogTemp, Warning, TEXT("i live"));
+}
